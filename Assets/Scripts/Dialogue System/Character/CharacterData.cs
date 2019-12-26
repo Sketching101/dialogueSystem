db536fed93f5f8e251721cc572896e9e4f28d109
unknown <animating101@gmail.com>
2019-12-26 12:44:20 +0300

    First steps

---
+++ Assets/Scripts/Dialogue System/Character/CharacterData.cs
@@ -0,0 +1,38 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue {
+    [CreateAssetMenu(fileName = "NewDialogueCharacter", menuName = "DialogueCharacter")]
+    public class CharacterData : ScriptableObject
+    {
+        public string charID;
+        public MoodSprite[] mood;
+        [BoxGroup("Character Information")]
+        public Bio charBio;
+
+    }
+
+    [System.Serializable]
+    public struct Bio
+    {
+        public string charName;
+        public int age;
+        public string backstoryPath;
+    }
+
+    [System.Serializable]
+    public struct MoodSprite
+    {
+        public Mood mood;
+
+        public Sprite lookLeftSprite;
+        public Sprite lookRightSprite;
+    }
+
+    public enum Mood
+    {
+        Happy, Sad, Angry, 
+    }
+}
---
+++ Assets/Scripts/Dialogue System/DialogueParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+namespace Dialogue
+{
+    public class DialogueParser : SerializedMonoBehaviour
+    {
+        public TextAsset parseDialogue;
+        public TextMeshProUGUI dialogueBox;
+
+        public string dialogueSetup;
+        public string[] dialogueTxt;
+
+        [DictionaryDrawerSettings(KeyLabel = "Character ID", ValueLabel = "Character Asset")]
+        public Dictionary<string, CharacterData> characters = new Dictionary<string, CharacterData>();
+
+        [Button]
+        public void InitializeDialogue()
+        {
+            string[] fullDialogue = parseDialogue.text.Split('#');
+
+            dialogueSetup = fullDialogue[1];
+            dialogueTxt = new string[fullDialogue.Length - 2];
+
+            for(int i = 2; i < fullDialogue.Length; i++)
+            {
+                dialogueTxt[i - 2] = fullDialogue[i];
+            }
+
+        }
+    }
+}
